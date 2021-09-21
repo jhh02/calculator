e003422d9648b77062bc681c9b647422eb64ffee
Juno Han <junhohan2@gmail.com>
2021-09-21 15:33:25 -0700

    completed logic

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+let displayValue = "";
+let tempVal = "";
+let value = "";
+let operator = "";
+let result = "";
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".calculation");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+
+let display = screen.textContent;
+
 function add(a, b) {
   return a + b;
 }
@@ -13,20 +25,109 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
   if (operator === "+") {
-    return add(a, b);
+    result = add(a, b);
+    screen.textContent = result;
+    displayValue = result;
   }
   if (operator === "-") {
-    return subtract(a, b);
+    result = subtract(a, b);
+    screen.textContent = result;
+    displayValue = result;
   }
-  if (operator === "*") {
-    return multiply(a, b);
+  if (operator === "×") {
+    result = multiply(a, b);
+    screen.textContent = result;
+    displayValue = result;
   }
-  if (operator === "/") {
-    return divide(a, b);
+  if (operator === "÷") {
+    result = divide(a, b);
+    screen.textContent = result.toFixed;
+    displayValue = result;
   }
   return;
 }
 
-function init() {}
+function clicked(e) {
+  const btn = e.target;
+  const btnValue = e.target.textContent;
+  // Number
+  if (btn.classList.contains("num")) {
+    // Store value
+    value += btnValue;
+    displayValue += btnValue;
+    screen.textContent = displayValue;
+    if (!displayValue.includes(".")) {
+      if (displayValue[0] === "0") {
+        displayValue = "";
+        screen.textContent = 0;
+      }
+    }
+  }
+  // Operator
+  if (btn.classList.contains("operator")) {
+    // operator not assigned
+    operator = btnValue;
+    if (
+      displayValue[displayValue.length - 1] === "+" ||
+      displayValue[displayValue.length - 1] === "-" ||
+      displayValue[displayValue.length - 1] === "×" ||
+      displayValue[displayValue.length - 1] === "÷"
+    ) {
+      displayValue = displayValue.slice(0, -1);
+    }
+    displayValue += operator;
+    screen.textContent = displayValue;
+    getTempVal(value);
+    value = "";
+  }
+  if (screen.textContent[0] === operator) {
+    displayValue = "";
+    screen.textContent = "";
+  }
+
+  if (btn.classList.contains("equal")) {
+    operate(operator, tempVal, +value);
+  }
+  if (btn.classList.contains("dot")) {
+    if (!displayValue) {
+      displayValue = "0.";
+      screen.textContent = displayValue;
+    }
+    if (!displayValue.includes(".")) {
+      displayValue += btn.textContent;
+    }
+  }
+  if (btn.classList.contains("delete")) {
+    backspace();
+  }
+}
+
+function backspace() {
+  screen.textContent = screen.textContent.slice(0, -1);
+  displayValue = +String(displayValue).slice(0, -1);
+  if (result) {
+    result = +String(result).slice(0, -1);
+  }
+}
+
+function clear() {
+  displayValue = "";
+  screen.textContent = "";
+  result = 0;
+  tempVal = "";
+  value = "";
+}
+
+function getTempVal(value) {
+  if (result) {
+    return (tempVal = result);
+  }
+  tempVal = +value;
+  return tempVal;
+}
+
+function init() {
+  btns.forEach((el) => el.addEventListener("click", clicked));
+}
 
 init();
