1819be0d61e9b81774c564bff836d4b5438e13d5
Juno Han <junhohan2@gmail.com>
2021-09-22 20:23:50 -0700

    fixed dot logic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let tempVal = "";
 let operator = "";
 let prevOperator = "";
 let result = 0;
+let count = 0;
 const arrOps = ["+", "-", "×", "÷"];
 
 const nums = document.querySelectorAll(".num");
@@ -48,10 +49,13 @@ function operate(a, b, operator) {
 }
 
 function numClicked(e) {
-  if (screen.textContent.includes(result)) {
+  const numVal = e.target.textContent;
+  if (
+    screen.textContent.includes(storedValue) &&
+    screen.textContent.includes(result)
+  ) {
     screen.textContent = "";
   }
-  let numVal = e.target.textContent;
   if (screen.textContent.includes("divide")) {
     screen.textContent = "";
     displayCal.textContent = "";
@@ -62,26 +66,40 @@ function numClicked(e) {
     displayCal.textContent += numVal;
     return;
   }
+
   if (screen.textContent[0] === "0") {
     if (screen.textContent.includes(".")) {
-      tempVal += numVal;
-      screen.textContent += numVal;
-      displayCal.textContent += numVal;
       return;
     } else {
       screen.textContent = "";
+      displayCal.textContent = "";
       tempVal = "";
     }
   }
-  if (prevOperator) {
-  }
+
   tempVal += numVal;
   screen.textContent += numVal;
   displayCal.textContent += numVal;
+  if (
+    screen.textContent.includes(
+      prevOperator ||
+        operator ||
+        displayCal.textContent.includes(prevOperator || operator)
+    )
+  ) {
+    const toArry = screen.textContent.split("");
+    let indexAfterOp = toArry.indexOf(operator) + 1;
+    if (screen.textContent[indexAfterOp] === "0") {
+      console.log("zero");
+    }
+  }
 }
 
 function backspace() {
-  if (screen.textContent[screen.textContent.length - 1] === operator) {
+  if (
+    screen.textContent[screen.textContent.length - 1] === operator ||
+    displayCal.textContent[displayCal.textContent.length - 1] === operator
+  ) {
     operator = "";
     prevOperator = "";
   } else if (screen.textContent === String(result)) {
@@ -98,8 +116,9 @@ function backspace() {
     tempVal = "";
     operator = "";
   }
+
   screen.textContent = screen.textContent.slice(0, -1);
-  displayCal.textContent = screen.textContent.slice(0, -1);
+  displayCal.textContent = displayCal.textContent.slice(0, -1);
 }
 
 function clear() {
@@ -107,8 +126,10 @@ function clear() {
   displayCal.textContent = "";
   result = 0;
   tempVal = "";
-  storedValue = "";
+  storedValue = null;
   operator = "";
+  prevOperator = "";
+  count = 0;
 }
 
 function operatorClicked(e) {
@@ -120,8 +141,10 @@ function operatorClicked(e) {
     arrOps.forEach((op) => {
       if (screen.textContent[screen.textContent.length - 1] === op) {
         screen.textContent = screen.textContent.slice(0, -1);
-        displayCal.textContent = displayCal.textContent.slice(0, -1);
         prevOperator = e.target.textContent;
+      }
+      if (displayCal.textContent[displayCal.textContent.length - 1] === op) {
+        displayCal.textContent = displayCal.textContent.slice(0, -1);
       }
       calculation(storedValue, tempVal, prevOperator);
       tempVal = "";
@@ -134,7 +157,6 @@ function operatorClicked(e) {
     displayCal.textContent += operator;
   }
   prevOperator = operator;
- 
 }
 
 function calculation(e) {
@@ -153,42 +175,61 @@ function calculation(e) {
       storedValue = 0;
       result = 0;
       tempVal = 0;
-    } else if (screen.textContent.includes(".")) {
+      count = 0;
+    } else if (screen.textContent.includes(".") || String(result.length) > 5) {
       result = Number.parseFloat(result).toFixed(2);
       storedValue = result;
       screen.textContent = result;
       displayCal.textContent = result;
+      count = 0;
     } else {
       screen.textContent = result;
       displayCal.textContent = result;
       storedValue = result;
       tempVal = "";
+      count = 0;
     }
   } else {
     if (!result) {
       screen.textContent = "";
       displayCal.textContent = "";
+      count = 0;
     } else {
       displayCal.textContent = result;
       screen.textContent = result;
       tempVal = screen.textContent;
+      count = 0;
     }
   }
 }
 
 function handleDot(e) {
-  if (!screen.textContent) {
-    screen.textContent = "0.";
-    tempVal = screen.textContent;
-  }
-  if (!screen.textContent.includes(".")) {
+  screen.textContent.split('').forEach(txt=> {
+    if(txt === '.'){
+      count++;
+    }
+  })
+  if (count < 2) {
+    if (!screen.textContent) {
+      screen.textContent += "0" + e.target.textContent;
+      displayCal.textContent = "0" + e.target.textContent;
+      tempVal = screen.textContent;
+      count++;
+      return;
+    }
+    if (!tempVal) {
+      screen.textContent = "0" + e.target.textContent;
+      displayCal.textContent += "0" + e.target.textContent;
+      count++;
+      return;
+    }
     screen.textContent += e.target.textContent;
+    displayCal.textContent += e.target.textContent;
     tempVal += e.target.textContent;
   }
 }
 
 function init() {
-  screen.textContent = 0;
   nums.forEach((el) => el.addEventListener("click", numClicked));
   operators.forEach((el) => el.addEventListener("click", operatorClicked));
   equal.addEventListener("click", calculation);
