9b9119f0b5ab4444457c830a4dc6ddb93d51f21f
Juno Han <junhohan2@gmail.com>
2021-09-23 23:05:32 -0700

    finised V0.0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Javascript calculator
+
+## V.0.01 Completed
+
+- refactored (not finished)
+- fixed decimal point number answear to hundredths
+- fixed display bugs showing on main display when delete button is clicked
+- fixed decimal point bugs
--- app.js
+++ app.js
@@ -1,250 +1,259 @@
-let storedValue = null;
-let tempVal = "";
-let operator = "";
-let prevOperator = "";
-let result = 0;
-let count = 0;
-const arrOps = ["+", "-", "×", "÷"];
-
-const nums = document.querySelectorAll(".num");
+const subDisplay = document.querySelector(".calculating");
+const mainDisplay = document.querySelector(".calculation");
+const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
-const screen = document.querySelector(".calculation");
 const clearBtn = document.querySelector(".clear");
 const deleteBtn = document.querySelector(".delete");
 const equal = document.querySelector(".equal");
-const displayCal = document.querySelector(".calculating");
-const dot = document.querySelector(".dot");
+const floatDot = document.querySelector(".floatdot");
 
-function minus(a, b) {
-  return a - b;
-}
-function plus(a, b) {
-  return a + b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  if (b === 0) {
-    return "You can't divide a number by 0";
-  } else {
-    return a / b;
-  }
-}
+const arrOps = ["+", "-", "×", "÷"];
+let numDotCount = 0;
+let result = 0;
+let storedValue = null;
+let tempValue = "";
+let operator = "";
+let prevOperator = "";
 
-function operate(a, b, operator) {
-  if (operator === "+") {
-    return plus(a, b);
-  }
-  if (operator === "-") {
-    return minus(a, b);
-  }
-  if (operator === "×") {
-    return multiply(a, b);
-  }
-  if (operator === "÷") {
-    return divide(a, b);
-  }
-  return;
+function operate(a, b, op) {
+  return op === "+"
+    ? a + b
+    : op === "-"
+    ? a - b
+    : op === "×"
+    ? a * b
+    : op === "÷" && b === 0
+    ? "You can't divide a number by 0"
+    : a / b;
 }
 
 function numClicked(e) {
   const numVal = e.target.textContent;
+
   if (
-    screen.textContent.includes(storedValue) &&
-    screen.textContent.includes(result)
+    mainDisplay.textContent.includes(storedValue) &&
+    mainDisplay.textContent.includes(result)
   ) {
-    screen.textContent = "";
+    mainDisplay.textContent = "";
   }
-  if (screen.textContent.includes("divide")) {
-    screen.textContent = "";
-    displayCal.textContent = "";
+
+  if (mainDisplay.textContent.includes("divide")) {
+    mainDisplay.textContent = "";
+    subDisplay.textContent = "";
   }
-  if (screen.textContent.includes(".")) {
-    tempVal += numVal;
-    screen.textContent += numVal;
-    displayCal.textContent += numVal;
+
+  if (mainDisplay.textContent.includes(".")) {
+    tempValue += numVal;
+    mainDisplay.textContent += numVal;
+    subDisplay.textContent += numVal;
     return;
   }
 
-  if (screen.textContent[0] === "0") {
-    if (screen.textContent.includes(".")) {
+  if (mainDisplay.textContent[0] === "0") {
+    if (mainDisplay.textContent.includes(".")) {
       return;
     } else {
-      screen.textContent = "";
-      displayCal.textContent = "";
-      tempVal = "";
+      mainDisplay.textContent = "";
+      subDisplay.textContent = "";
+      tempValue = "";
     }
   }
 
-  tempVal += numVal;
-  screen.textContent += numVal;
-  displayCal.textContent += numVal;
-  if (
-    screen.textContent.includes(
-      prevOperator ||
-        operator ||
-        displayCal.textContent.includes(prevOperator || operator)
-    )
-  ) {
-    const toArry = screen.textContent.split("");
-    let indexAfterOp = toArry.indexOf(operator) + 1;
-    if (screen.textContent[indexAfterOp] === "0") {
-      console.log("zero");
-    }
-  }
+  tempValue += numVal;
+  mainDisplay.textContent += numVal;
+  subDisplay.textContent += numVal;
 }
 
 function backspace() {
   if (
-    screen.textContent[screen.textContent.length - 1] === operator ||
-    displayCal.textContent[displayCal.textContent.length - 1] === operator
+    subDisplay.textContent[subDisplay.textContent.length - 1] === operator &&
+    mainDisplay.textContent[mainDisplay.textContent.length - 1] ===
+      String(result)[String(result).length - 1]
+  ) {
+    subDisplay.textContent = subDisplay.textContent.slice(0, -1);
+    operator = "";
+    prevOperator = "";
+    return;
+  }
+
+  if (
+    mainDisplay.textContent[mainDisplay.textContent.length - 1] === operator ||
+    subDisplay.textContent[subDisplay.textContent.length - 1] === operator
   ) {
     operator = "";
     prevOperator = "";
-  } else if (screen.textContent === String(result)) {
+  } else if (mainDisplay.textContent === String(result)) {
     result = +String(result).slice(0, -1);
     storedValue = +String(storedValue).slice(0, -1);
   } else if (
-    screen.textContent[screen.textContent.length - 1] ===
-    tempVal[tempVal.length - 1]
+    mainDisplay.textContent[mainDisplay.textContent.length - 1] ===
+    tempValue[tempValue.length - 1]
   ) {
-    tempVal = tempVal.slice(0, -1);
+    tempValue = tempValue.slice(0, -1);
   }
-  if (screen.textContent === "") {
+
+  if (mainDisplay.textContent === "") {
     storedValue = "";
-    tempVal = "";
+    tempValue = "";
     operator = "";
   }
 
-  screen.textContent = screen.textContent.slice(0, -1);
-  displayCal.textContent = displayCal.textContent.slice(0, -1);
+  mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+  subDisplay.textContent = subDisplay.textContent.slice(0, -1);
 }
 
 function clear() {
-  screen.textContent = "0";
-  displayCal.textContent = "";
   result = 0;
-  tempVal = "";
+  numDotCount = 0;
   storedValue = null;
+  tempValue = "";
   operator = "";
   prevOperator = "";
-  count = 0;
+  mainDisplay.textContent = "0";
+  subDisplay.textContent = "";
 }
 
 function operatorClicked(e) {
-  if(tempVal === '0.' || displayCal.textContent === '0.' || tempVal === '0' || screen.textContent === '0'){
-    tempVal = ''
-    screen.textContent = '0'
-    displayCal.textContent = ''
-    prevOperator = ''
-    operator = ''
-    count = 0;
-    return
+  if (
+    tempValue === "0." ||
+    subDisplay.textContent === "0." ||
+    tempValue === "0" ||
+    mainDisplay.textContent === "0"
+  ) {
+    numDotCount = 0;
+    tempValue = "";
+    operator = "";
+    prevOperator = "";
+    mainDisplay.textContent = "0";
+    subDisplay.textContent = "";
+    return;
   }
+
   if (!prevOperator) {
-    storedValue = screen.textContent;
-    tempVal = "";
+    storedValue = mainDisplay.textContent;
+    tempValue = "";
   }
+
   if (prevOperator) {
     arrOps.forEach((op) => {
-      if (screen.textContent[screen.textContent.length - 1] === op) {
-        screen.textContent = screen.textContent.slice(0, -1);
+      if (mainDisplay.textContent[mainDisplay.textContent.length - 1] === op) {
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
         prevOperator = e.target.textContent;
       }
-      if (displayCal.textContent[displayCal.textContent.length - 1] === op) {
-        displayCal.textContent = displayCal.textContent.slice(0, -1);
+
+      if (subDisplay.textContent[subDisplay.textContent.length - 1] === op) {
+        subDisplay.textContent = subDisplay.textContent.slice(0, -1);
       }
-      calculation(storedValue, tempVal, prevOperator);
-      tempVal = "";
+
+      calculation(storedValue, tempValue, prevOperator);
+      tempValue = "";
     });
+
     operator = e.target.textContent;
-    displayCal.textContent += operator;
+    subDisplay.textContent += operator;
   } else {
     operator = e.target.textContent;
-    screen.textContent += operator;
-    displayCal.textContent += operator;
+    mainDisplay.textContent += operator;
+    subDisplay.textContent += operator;
   }
+
   prevOperator = operator;
 }
 
 function calculation(e) {
-  if (!tempVal) {
+  if (!tempValue) {
     return;
   }
-  if (tempVal && !storedValue && !result) {
-    screen.textContent = tempVal;
+
+  if (tempValue && !storedValue && !result) {
+    mainDisplay.textContent = tempValue;
     return;
   }
-  if (tempVal && operator) {
-    result = operate(+storedValue, +tempVal, operator);
+
+  if (tempValue && operator) {
+    result = operate(+storedValue, +tempValue, operator);
     operator = "";
     if (isNaN(result)) {
-      screen.textContent = "You can't divide a number by 0";
       storedValue = 0;
       result = 0;
-      tempVal = 0;
-      count = 0;
-    } else if (screen.textContent.includes(".") || String(result.length) > 5) {
+      tempValue = 0;
+      numDotCount = 0;
+      mainDisplay.textContent = "You can't divide a number by 0";
+    } else if (
+      mainDisplay.textContent.includes(".") ||
+      String(result.length) > 4
+    ) {
+      console.log('working');
       result = Number.parseFloat(result).toFixed(2);
+      numDotCount = 0;
       storedValue = result;
-      screen.textContent = result;
-      displayCal.textContent = result;
-      count = 0;
+      subDisplay.textContent = result;
+      mainDisplay.textContent = result;
     } else {
-      screen.textContent = result;
-      displayCal.textContent = result;
+      console.log('not working 1');
+      tempValue = "";
+      numDotCount = 0;
+      result = Number.parseFloat(result).toFixed(2)
       storedValue = result;
-      tempVal = "";
-      count = 0;
+      mainDisplay.textContent = result;
+      subDisplay.textContent = result;
     }
   } else {
     if (!result) {
-      screen.textContent = "";
-      displayCal.textContent = "";
-      count = 0;
+      console.log('not working 2');
+      numDotCount = 0;
+      mainDisplay.textContent = "";
+      subDisplay.textContent = "";
     } else {
-      displayCal.textContent = result;
-      screen.textContent = result;
-      tempVal = screen.textContent;
-      count = 0;
+      console.log('not working 3');
+      numDotCount = 0;
+      subDisplay.textContent = result;
+      mainDisplay.textContent = result;
+      tempValue = mainDisplay.textContent;
     }
   }
 }
 
-function handleDot(e) {
-  screen.textContent.split('').forEach(txt=> {
-    if(txt === '.'){
-      count++;
+function handlefloatDot(e) {
+  if (mainDisplay.textContent[mainDisplay.textContent.length - 1] === "." || tempValue.split('').includes('.')) {
+    return;
+  }
+
+  mainDisplay.textContent.split("").forEach((txt) => {
+    if (txt === ".") {
+      numDotCount++;
     }
-  })
-  if (count < 3) {
-    if (!screen.textContent) {
-      screen.textContent += "0" + e.target.textContent;
-      displayCal.textContent = "0" + e.target.textContent;
-      tempVal = screen.textContent;
-      count++;
+  });
+
+  if (numDotCount < 3) {
+    if (!mainDisplay.textContent) {
+      numDotCount++;
+      tempValue = mainDisplay.textContent;
+      mainDisplay.textContent += "0" + e.target.textContent;
+      subDisplay.textContent = "0" + e.target.textContent;
       return;
     }
-    if (!tempVal) {
-      screen.textContent = "0" + e.target.textContent;
-      displayCal.textContent += "0" + e.target.textContent;
-      count++;
+
+    if (!tempValue) {
+      numDotCount++;
+      mainDisplay.textContent = "0" + e.target.textContent;
+      subDisplay.textContent += "0" + e.target.textContent;
       return;
     }
-    screen.textContent += e.target.textContent;
-    displayCal.textContent += e.target.textContent;
-    tempVal += e.target.textContent;
+    
+    tempValue += e.target.textContent;
+    mainDisplay.textContent += e.target.textContent;
+    subDisplay.textContent += e.target.textContent;
   }
 }
 
 function init() {
-  nums.forEach((el) => el.addEventListener("click", numClicked));
-  operators.forEach((el) => el.addEventListener("click", operatorClicked));
-  equal.addEventListener("click", calculation);
   clearBtn.addEventListener("click", clear);
+  equal.addEventListener("click", calculation);
   deleteBtn.addEventListener("click", backspace);
-  dot.addEventListener("click", handleDot);
+  floatDot.addEventListener("click", handlefloatDot);
+  numbers.forEach((el) => el.addEventListener("click", numClicked));
+  operators.forEach((el) => el.addEventListener("click", operatorClicked));
 }
 
 init();
